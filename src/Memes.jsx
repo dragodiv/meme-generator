201764f5ae9ab2875cb08b63100ea3d8251b2165
dragodiv <dragodiv18@gmail.com>
2023-11-16 15:17:09 +0530

    nothing

--- src/Memes.jsx
+++ src/Memes.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 // import MemeData from "./MemeData";
 
-export default function Memes(){
+export default function Memes() {
 
     let url
-    const [Meme, setMeme]=React.useState({
-        topText:"",
-        bottomText:"",
-        randomImage:"https://i.imgflip.com/265k.jpg"
+    const [Meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "https://i.imgflip.com/265k.jpg"
     })
 
     const [allMemes, setAllMemes] = React.useState([])
@@ -18,33 +18,33 @@ export default function Memes(){
             .then(data => setAllMemes(data.data.memes))
     }, [])
 
-    function getNewImage(){
+    function getNewImage() {
         const randomNumber = Math.floor(Math.random() * allMemes.length);
         url = allMemes[randomNumber].url
-        setMeme(prevMeme=>({
+        setMeme(prevMeme => ({
             ...prevMeme,
-            randomImage:url
+            randomImage: url
         }))
         console.log(url)
     }
 
     function handleChange(event) {
-        const {name, value} = event.target
+        const { name, value } = event.target
         setMeme(prevMeme => ({
             ...prevMeme,
             [name]: value
         }))
     }
 
-    return(
+    return (
         <div className="form">
             <div className="txt-f">
-                <input type="text" name="topText" id="top-text" value={Meme.topText} placeholder="Top Text" onChange={handleChange}/>
-                <input type="text" name="bottomText" id="bottom-text" value={Meme.bottomText} placeholder="Bottom Text" onChange={handleChange}/>
+                <input type="text" name="topText" id="top-text" value={Meme.topText} placeholder="Top Text" onChange={handleChange} />
+                <input type="text" name="bottomText" id="bottom-text" value={Meme.bottomText} placeholder="Bottom Text" onChange={handleChange} />
             </div>
             <button className="meme-btn" onClick={getNewImage}>Get a new meme image 🖼</button>
             <div className="meme">
-                <img src={Meme.randomImage} alt="" className="meme-image"/>
+                <img src={Meme.randomImage} alt="" className="meme-image" />
                 <h2 className="top-txt">{Meme.topText}</h2>
                 <h2 className="bottom-txt">{Meme.bottomText}</h2>
             </div>
