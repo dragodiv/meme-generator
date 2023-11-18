bae5aabc1f05aed4a532bce9cde576c1576eaf6c
dragodiv <dragodiv18@gmail.com>
2023-11-19 01:37:39 +0530

    update

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar(){
                 <img src="/logo.png" alt="" className="meme-logo" />
                 <h2>Meme Generator</h2>
             </div>
-            <h3 className="nav-txt">React Project - 3</h3>
+            {/* <h3 className="nav-txt">React Project - 3</h3> */}
         </div>
     )
 }
